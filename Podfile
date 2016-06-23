5130a6c4c608f9f3f4a1bd097093a30f52bda96f
Simone Basso <bassosimone@gmail.com>
2016-06-23 17:46:40 +0200

    Version 1.2
    1) use cryptography for https
    
    2) use `integration/ndt-ooni-callback-error` MK's branch
    
    3) fixed width font in the output
    
    4) add certificate bundle from libressl
    
    5) add GeoIP databases

--- Podfile
+++ Podfile
@@ -3,5 +3,5 @@ platform :ios, '9.0'
 target 'measurement-kit-test' do
   pod 'measurement_kit',
     :git => 'https://github.com/measurement-kit/measurement-kit.git',
-    :branch => 'feature/no-encryption'
+    :branch => 'integration/ndt-ooni-callback-error'
 end
--- measurement-kit-test/NetworkMeasurement.mm
+++ measurement-kit-test/NetworkMeasurement.mm
@@ -27,14 +27,26 @@
 }
 
 -(void) run {
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *geoip_asn = [bundle pathForResource:@"GeoIPASNum" ofType:@"dat"];
+    NSString *geoip_country = [bundle pathForResource:@"GeoIP" ofType:@"dat"];
+    NSString *ca_cert = [bundle pathForResource:@"cacert" ofType:@"pem"];
+
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                                         NSUserDomainMask, YES);
+    NSString *docs_dir = [paths objectAtIndex:0];
+    NSString *ofile = [NSString stringWithFormat:@"%@/output.json", docs_dir];
+
     // Note: the emulator does not cope well with receiving
     // a signal of type SIGPIPE when the debugger is attached
     // See http://stackoverflow.com/questions/1294436
 
+    // XXX: MK_LOG_DEBUG2 sends app to deadlock?!
+
     mk::ndt::NdtTest()
-        .set_options("mlabns/base_url", "http://mlab-ns.appspot.com/")
         .set_options("test_suite", MK_NDT_DOWNLOAD)
         .set_verbosity(MK_LOG_INFO)
+        .set_output_file_path([ofile UTF8String])
         .on_log([self](uint32_t, const char *s) {
             NSString *current = [NSString stringWithFormat:@"%@",
                                  [NSString stringWithUTF8String:s]];
@@ -45,7 +57,10 @@
                  postNotificationName:@"refreshLog" object:nil];
             });
         })
+        .set_options("net/ca_bundle_path", [ca_cert UTF8String])
         .set_options("dns/nameserver", "8.8.8.8")
+        .set_options("geoip_country_path", [geoip_country UTF8String])
+        .set_options("geoip_asn_path", [geoip_asn UTF8String])
         .run([self]() {
             dispatch_async(dispatch_get_main_queue(), ^{
                 self.finished = TRUE;
